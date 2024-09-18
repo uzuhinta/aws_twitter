84c3e3d6bf4f486478c9e8034c8e4a86f9f3219e
Quân Nguyễn Bá <uzumakiruto017@Quans-MacBook-Pro.local>
2024-09-18 08:45:08 +0700

    retweetOf

---
+++ functions/reply.js
@@ -0,0 +1,110 @@
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  PutItemCommand,
+  TransactWriteItemsCommand,
+} from '@aws-sdk/client-dynamodb';
+import { marshall } from '@aws-sdk/util-dynamodb';
+import { ulid } from 'ulid';
+import { getTweetById } from '../lib/tweets';
+import _ from 'lodash';
+
+const client = new DynamoDBClient();
+const { USERS_TABLE_NAME, TWEETS_TABLE_NAME, TIMELINES_TABLE_NAME } =
+  process.env;
+export const handler = async (event) => {
+  console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+
+  const { tweetId, text } = event.arguments;
+  const { username } = event.identity;
+  const id = ulid();
+  const timestamp = new Date().toJSON();
+
+  const storedTweet = await getTweetById(tweetId);
+
+  if (!storedTweet) throw new Error('Not found');
+
+  const inReplyToUserIds = await getUserIdsToReplyTo(tweet);
+
+  const tweet = {
+    __typename: 'Reply',
+    id,
+    creator: username,
+    createdAt: timestamp,
+    inReplyToTweetId: tweetId,
+    inReplyToUserIds,
+    text,
+    replies: 0,
+    likes: 0,
+    retweets: 0,
+  };
+
+  console.log('tweet: \n' + JSON.stringify(tweet, null, 2));
+
+  const transactItems = [
+    {
+      Put: {
+        TableName: TWEETS_TABLE_NAME,
+        Item: marshall(tweet),
+      },
+    },
+    {
+      Update: {
+        TableName: TWEETS_TABLE_NAME,
+        Key: marshall({
+          id: tweetId,
+        }),
+        UpdateExpression: 'ADD replies :one',
+        ConditionExpression: 'attribute_exists(id)',
+        ExpressionAttributeValues: marshall({
+          ':one': 1,
+        }),
+      },
+    },
+    {
+      Update: {
+        TableName: USERS_TABLE_NAME,
+        Key: marshall({
+          id: username,
+        }),
+        UpdateExpression: 'ADD tweetsCount :one',
+        ConditionExpression: 'attribute_exists(id)',
+        ExpressionAttributeValues: marshall({
+          ':one': 1,
+        }),
+      },
+    },
+    {
+      Put: {
+        TableName: TIMELINES_TABLE_NAME,
+        Item: marshall({
+          userId: username,
+          tweetId: id,
+          inReplyToTweetId: tweetId,
+          inReplyToUserIds,
+          timestamp,
+        }),
+      },
+    },
+  ];
+
+  await client.send(
+    new TransactWriteItemsCommand({
+      TransactItems: transactItems,
+    })
+  );
+
+  return true;
+};
+
+async function getUserIdsToReplyTo(tweet) {
+  let userIds = [tweet.creator];
+  if (tweet.__typename === TweetTypes.REPLY) {
+    userIds = userIds.concat(tweet.inReplyToUserIds);
+  } else if (tweet.__typename === TweetTypes.RETWEET) {
+    const retweetOf = await getTweetById(tweet.retweetOf);
+    userIds = userIds.concat(await getUserIdsToReplyTo(retweetOf));
+  }
+
+  return _.uniq(userIds);
+}
