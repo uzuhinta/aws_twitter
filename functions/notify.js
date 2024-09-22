5d5acabd8a8ee0c390c4ceff1ea648bf4560fff3
Quân Nguyễn Bá <uzumakiruto017@Quans-MacBook-Pro.local>
2024-09-22 11:50:05 +0700

    notify mentioned

--- functions/notify.js
+++ functions/notify.js
@@ -1,11 +1,14 @@
-import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb';
-import { unmarshall } from '@aws-sdk/util-dynamodb';
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  QueryCommand,
+} from '@aws-sdk/client-dynamodb';
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
 import { SignatureV4 } from '@aws-sdk/signature-v4';
-import { createRequest } from '@aws-sdk/util-create-request';
 import { Sha256 } from '@aws-sdk/crypto-sha256-js';
-import gql from 'graphql-tag';
 import { ulid } from 'ulid';
 import { defaultProvider } from '@aws-sdk/credential-provider-node';
+import _ from 'lodash';
 
 const {
   GRAPHQL_API_URL,
@@ -77,6 +80,13 @@ export const handler = async (event) => {
         //   await notifyReply(tweet.inReplyToUserIds, tweet)
         //   break
       }
+
+      if (tweet.text) {
+        const mentions = extractMentions(tweet.text);
+        if (!_.isEmpty(mentions)) {
+          await notifyMentioned(mentions, tweet);
+        }
+      }
     }
   }
 };
@@ -143,4 +153,80 @@ async function getTweetById(tweetId) {
   console.log('res', res);
 
   return res.Item;
+}
+
+async function notifyMentioned(screenNames, tweet) {
+  const promises = screenNames.map(async (screenName) => {
+    const user = await getUserByScreenName(screenName.replace('@', ''));
+    if (!user) {
+      return;
+    }
+
+    await mutate(
+      `mutation notifyMentioned(
+      $id: ID!
+      $userId: ID!
+      $mentionedBy: ID!
+      $mentionedByTweetId: ID!
+    ) {
+      notifyMentioned(
+        id: $id
+        userId: $userId
+        mentionedBy: $mentionedBy
+        mentionedByTweetId: $mentionedByTweetId
+      ) {
+        __typename
+        ... on Mentioned {
+          id
+          type
+          userId
+          mentionedBy
+          mentionedByTweetId
+          createdAt
+        }
+      }
+    }`,
+      {
+        id: ulid(),
+        userId: user.id,
+        mentionedBy: tweet.creator,
+        mentionedByTweetId: tweet.id,
+      }
+    );
+  });
+
+  await Promise.all(promises);
+}
+
+function extractMentions(text) {
+  const mentions = new Set();
+  const regex = /@\w+/gm;
+
+  let m;
+  while ((m = regex.exec(text)) !== null) {
+    // this is necessary to avoid infinite loops with zero-width matches
+    if (m.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+
+    m.forEach((match) => mentions.add(match));
+  }
+
+  return Array.from(mentions);
+}
+
+async function getUserByScreenName(screenName) {
+  const resp = await client.send(
+    new QueryCommand({
+      TableName: USERS_TABLE_NAME,
+      KeyConditionExpression: 'screenName = :screenName',
+      ExpressionAttributeValues: marshall({
+        ':screenName': screenName,
+      }),
+      IndexName: 'byScreenName',
+      Limit: 1,
+    })
+  );
+
+  return _.get(resp, 'Items.0');
 }
